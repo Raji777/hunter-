391f04399b1a18a56f3223ac44a42069aced467c
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-03-07 18:00:41 +0530

    4.cpp

---
+++ 4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n,a[i],i;
+cout<<"enter n";
+cin>>n;
+cout<<"enter array elements";
+for(i=0;i<=n;i++)
+{
+	cout<<a[i];
+	}
+for(int i=0;i<a[i];i++)
+for(int j=0;j<a[j];j++)
+{
+	if(a[i]!=a[j])
+	{
+		cout<<a[i];
+	}
+}
+	return 0;
+}
