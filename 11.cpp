48903e35df58da974d286d59cbdc3d44029e694e
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-03-10 11:52:39 +0530

    11.cpp

---
+++ 11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+ char str[100],temp;
+ int i,j=0;
+ cout<<"Enter any the string :";
+ cin>>str;  
+ i=0;
+ j=strlen(str)-1;
+  while(i<j)
+  {
+    	if(str[i]=="")
+  {	
+     temp=str[i];
+   str[i]=str[j];
+   str[j]=temp;
+   i++;
+   j--;
+   }
+  }
+ cout<<"Reverse string is: "<<str;
+	return 0;
+}
